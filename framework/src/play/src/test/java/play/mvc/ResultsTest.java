35f489d7ad871743cd108340192463f14f7b5e89
Ben Fletcher <bjfletcher@gmail.com>
2015-06-27 01:09:38 +0100

    Added tests for sendFile, accept bytes header & byte range response

--- framework/src/play/src/test/java/play/mvc/ResultsTest.java
+++ framework/src/play/src/test/java/play/mvc/ResultsTest.java
@@ -2,6 +2,8 @@ package play.mvc;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,6 +16,51 @@ public class ResultsTest {
   @Test(expected = NullPointerException.class)
   public void shouldThrowNullPointerExceptionIfPathIsNull() throws IOException {
     Results.ok().sendPath(null);
+  }
+
+  @Test
+  public void sendFile() throws IOException {
+    File file = new File("test.tmp");
+    try {
+      file.createNewFile();
+      Results.Status status = Results.ok(file);
+
+      assertEquals(status.status(), Http.Status.OK);
+      assertEquals(status.header(Result.CONTENT_DISPOSITION), "attachment; filename=\"test.tmp\"");
+    } finally {
+      file.delete();
+    }
+  }
+
+  @Test
+  public void sendFileAcceptRanges() throws IOException {
+    File file = new File("test.tmp");
+    try {
+      file.createNewFile();
+      Results.Status status = Results.ok(file);
+
+      assertEquals(status.status(), Http.Status.OK);
+      assertEquals(status.header(Result.ACCEPT_RANGES), "bytes");
+    } finally {
+      file.delete();
+    }
+  }
+
+  @Test
+  public void sendFileAsPartialContent() throws IOException {
+    File file = new File("test.tmp");
+    try {
+      FileOutputStream out = new FileOutputStream(file);
+      out.write(new byte[]{ 1, 2, 3, 4, 5, 6, 7 });
+      out.close();
+      Results.Status status = Results.ok(file, false, null, "bytes=2-4");
+
+      assertEquals(status.status(), Http.Status.PARTIAL_CONTENT);
+      assertEquals(status.header(Result.CONTENT_RANGE), "bytes 2-4/7");
+      assertEquals(status.header(Result.CONTENT_LENGTH), "3");
+    } finally {
+      file.delete();
+    }
   }
 
   @Test
